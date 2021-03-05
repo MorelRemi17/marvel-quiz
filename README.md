610caf09bb8fa1bf3929d7898a90c231fe2bfc09
unknown <mrl.remi@gmail.com>
2021-03-05 10:20:47 +0100

    addition of btn effect

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 Technologies : 
 
 - ReactJs
+    - useRef
+    - useEffect
+    - useState
+    - Fragment
 - Firebase
 - Marvel API
 - Axios
-- ... 
+- ...
 
 Création d'une application permettant de faire des quiz sur l'univers marvel, il y aura la possibilité de créer son compte avec mots de passe ect.
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef, useState, Fragment }from 'react';
 
 const Landing = () => {
+    const [btn, setBtn] = useState(false);
+    const refWolverine = useRef(null);
+
+    useEffect(() =>{
+        refWolverine.current.classList.add("startingImg");
+        setTimeout(() =>{
+            refWolverine.current.classList.remove("startingImg");
+            setBtn(true)
+        }, 1000);
+    }, []);
+
+    const displayBtn = btn && (
+      <Fragment>
+        <div className="leftBox">
+          <button className="btn-welcome"> Inscription </button>
+        </div>
+        <div className="rightBox">
+          <button className="btn-welcome"> connexion </button>
+        </div>
+      </Fragment>
+    );
+
     return (
-        <main className="welcomePage">
-            <div className="leftBox">
-            <button className="btn-welcome"> Inscription </button>
-            </div>
-            <div className="rightBox">
-            <button className="btn-welcome"> connexion </button>
-            </div>
+        <main ref={refWolverine} className="welcomePage">
+            {displayBtn}
         </main>
     );
 };
