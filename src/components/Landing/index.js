28f5c3e52e99d115537734447fb964cd6a629f7b
unknown <mrl.remi@gmail.com>
2021-03-05 10:00:34 +0100

    addition of button welcome

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,6 +3,12 @@ import React from 'react';
 const Landing = () => {
     return (
         <main className="welcomePage">
+            <div className="leftBox">
+            <button className="btn-welcome"> Inscription </button>
+            </div>
+            <div className="rightBox">
+            <button className="btn-welcome"> connexion </button>
+            </div>
         </main>
     );
 };
