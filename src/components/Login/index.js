babf509ab72fff1a2519c29b24d3e7e390bcfacb
unknown <mrl.remi@gmail.com>
2021-03-09 10:35:56 +0100

    login done

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,30 +1,64 @@
-import React, {useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import firebaseContext from '../Firebase/context';
 
+const Login = ( props ) => {
+  // ! Commenter le code 13 a 6min.
 
-const Login = () => {
+  // * Cela va nous permettre d'avoir accés au méthodes présente dans firebase . 
+  const firebase = useContext(firebaseContext);
 
-  // ! Commenter le code 13 a 6min. 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [btn, setBtn] = useState(false);
+  const [error, setError] = useState('');
 
-  const [ email , setEmail ] = useState('');
-  const [ password , setPassword ] = useState('');
-  const [ btn, setBtn] = useState(false);
-
-  // * Le tableau vide à la fin de la fonction est équivalent au composentDidMount ce qui aura comme effet que cette fonction fléché ne vas s'enclancher qu'une seule fois 
+  /*
+   * Nous allons ici faire une petite securité pour verifier que le mots de passe fait plus de 6 caractéres et si email est différents d'une chaine de caractére vide.
+   * Le else if ici vas nous permettre de disabled le btn de nouveau si après avoir mis 6 caractéres on en enléves. 
+   * Le tableau vide à la fin de la fonction est équivalent au composentDidMount ce qui aura comme effet que cette fonction fléché ne vas s'enclancher qu'une seule fois, on ajoute email et password pour verifier leurs changement.
+   */
   useEffect(() => {
+    if (password.length > 5 && email !== "") {
+      setBtn(true);
+    } else if (btn) {
+        setBtn(false);
+    }
+  }, [password, email, btn]);
 
-  }, []) 
+  /*
+  * Je vais gérer la soumission de mon fomulaire ici. 
+  * firebase.loginUser(email, password) nous sert a verifier que les logins sont bon et loginUser necessite 2 paramétres ( email password ). 
+  * En cas de succés ( .then ) on vide les setEmail et setPassword et on nous redirige vers la page welcome
+  * 
+  */
+  const handleSubmit = e => {
+    e.preventDefault();
+    firebase.loginUser(email, password) 
+    .then(user => {
+      setEmail('');
+      setPassword('');
+      props.history.push('/welcome')
+    })
+    .catch(error => {
+      setError(error);
+      setEmail('');
+      setPassword('');
+    })
+  }
+
   return (
     <div className="signUpLoginBox">
       <div className="slContainer">
-      <div className="formBoxLeftLogin"></div>
+        <div className="formBoxLeftLogin"></div>
         <div className="formBoxRight">
           <div className="formContent">
+            { error !== '' && <span>{error.message}</span>}
             <h2>Connexion</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="inputBox">
                 <input
-                  onChange={e => setEmail(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                   value={email}
                   type="text"
                   autoComplete="off"
@@ -34,7 +68,7 @@ const Login = () => {
               </div>
               <div className="inputBox">
                 <input
-                  onChange={e => setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                   value={password}
                   type="password"
                   autoComplete="off"
@@ -42,9 +76,14 @@ const Login = () => {
                 />
                 <label htmlFor="password">Mot de passe</label>
               </div>
+
+              {btn ? <button>Connexion</button> : <button disabled> Connexion</button>}
+
             </form>
             <div className="linkContainer">
-              <Link className="simpleLink" to="/signup">Pas de compte ? Inscrivez vous ! </Link>
+              <Link className="simpleLink" to="/signup">
+                Pas de compte ? Inscrivez vous !{" "}
+              </Link>
             </div>
           </div>
         </div>
