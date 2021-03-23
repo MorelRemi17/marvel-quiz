a0a2b31d7ecd20613e4eb89adfa4de3589b480d1
unknown <mrl.remi@gmail.com>
2021-03-23 10:08:25 +0100

    Managing displays on the QuizOver part 2 component when succed

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,13 +1,41 @@
 import React, { Fragment, useEffect, useState } from "react";
 
 const QuizOver = React.forwardRef((props, ref) => {
+
   const { levelNames, score, maxQuestions, quizLevel, percent } = props;
+
   const [asked, setAsked] = useState([]);
+
   useEffect(() => {
     setAsked(ref.current);
   }, [ref]);
+
   const averageGrade = maxQuestions / 2;
-  const decision = score >= averageGrade;
+
+  const decision =
+    score >= averageGrade ? (
+      <Fragment>
+        <div className="stepsBtnContainer">
+        {quizLevel < levelNames.length ? (
+          <Fragment>
+            <p className="successMsg">Bravo, passez au niveau suivant !</p>
+            <button className="btnResult success">Niveau suivant </button>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <p className="successMsg">Bravo !</p>
+            <button className="btnResult gameOver">Niveau suivant </button>
+          </Fragment>
+        )}
+        </div>
+        <div className="percentage">
+        <div className="progressPercent">Réussite {percent} % </div>
+        <div className="progressPercent"> Note {score}/{maxQuestions} ! </div>
+      </div>
+      </Fragment>
+    ) : (
+      <Fragment></Fragment>
+    );
   const questionAnswer = asked.map((question) => {
     return (
       <tr key={question.id}>
@@ -21,14 +49,16 @@ const QuizOver = React.forwardRef((props, ref) => {
   });
   return (
     <Fragment>
-      <div className="stepsBtnContainer">
+{/*       <div className="stepsBtnContainer">
         <p className="successMsg">Bravo !</p>{" "}
         <button className="btnResult success">Niveau suivant </button>
       </div>
+
       <div className="percentage">
         <div className="progressPercent">Réussite 10 % </div>
         <div className="progressPercent"> Note 01/10 ! </div>
-      </div>
+      </div> */}
+      {decision}
       <hr />
       <p> Les réponses aux questions posées :</p>
       <div className="answerContainer">
